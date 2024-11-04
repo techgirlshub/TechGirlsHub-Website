9d4f8c5452f3f8b97c42a78192063fd00c60fd35
Palesa <devnhack@gmail.com>
2024-11-04 09:44:01 +0200

    Removed unused heros, PhpMailer folder, and duplicate imports of modules

--- contactUs.php
+++ contactUs.php
@@ -343,10 +343,5 @@
           }
 
         </script>
-         <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
-         <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.3.0/js/bootstrap.min.js"></script>
-
-
     </body>
 </html> 
